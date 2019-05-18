6109ca5c86baf3a50d52a98bb6b924ad6cf48d9b
chocoboeater <953020309@qq.com>
2019-05-18 10:09:49 +0800

    add project

---
+++ src/cn/xinxing/service/news/NewService.java
@@ -0,0 +1,13 @@
+package cn.xinxing.service.news;
+
+import java.util.List;
+
+import cn.xinxing.model.News;
+
+/**
+ * 
+ */
+public interface NewService {
+	public List<News> getAllNews();
+	public void addData(String _id, String _ip);
+}
